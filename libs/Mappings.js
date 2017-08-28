0b164c086521f9a185f31910faff0f928122255f
Conrado Rodriguez <conrado.rodriguez@visual-meta.com>
2017-08-28 19:07:15 +0200

    Cleanup entry point

--- libs/Mappings.js
+++ libs/Mappings.js
@@ -1,25 +1,24 @@
-var util = require('util');	
+var util = require('util');
 var events = require('events');
 var csv = require('ya-csv');
 
 function Mappings() {
-  events.EventEmitter.call(this); 
-  this.process = function(filePath)
-  {
-  	var reader = csv.createCsvFileReader(filePath);
-	var map = {};
-	var self = this;
-	reader.addListener('data', function(data) {
-		var imageName = data[2];
-		var label = data[4];
-	    if(!map.hasOwnProperty(imageName) && label) {
-	    	map[imageName] = label;
-	    }
-	});	
-	reader.addListener('end', function() {
-		self.emit('end', map); 
-	});
-  }
+	events.EventEmitter.call(this);
+	this.process = function(filePath) {
+		var reader = csv.createCsvFileReader(filePath);
+		var map = {};
+		var self = this;
+		reader.addListener('data', function(data) {
+			var imageName = data[2];
+			var label = data[4];
+			if (!map.hasOwnProperty(imageName) && label) {
+				map[imageName] = label;
+			}
+		});
+		reader.addListener('end', function() {
+			self.emit('end', map);
+		});
+	}
 }
 util.inherits(Mappings, events.EventEmitter);
 module.exports = Mappings;
